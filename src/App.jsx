e5dca583fa2f2bd5eabf0788a0c817636eefde5f
ShaikhFaizan2006 <engg.shaikhfaizan@gmail.com>
2026-05-02 16:36:17 +0530

    add the file

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
  
   return (
     <>
-    <h1 style={{fontSize:100, textAlign:'center', marginTop:600}}> Lets Learn Together</h1>
+    <h1 style={{fontSize:90, textAlign:'center', marginTop:300}}> Lets Learn Together</h1>
     <h2 style={{textAlign:'center', marginTop:50}}>Hello whats Going On!</h2>
     
     </>
