7430abd19ee34ecf6e5cb0a68872a46c4b8b3a0f
ShaikhFaizan2006 <engg.shaikhfaizan@gmail.com>
2026-05-02 16:47:22 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
     <h1 style={{fontSize:90, textAlign:'center', marginTop:300}}> Lets Learn Together</h1>
-    <h2 style={{textAlign:'center', marginTop:50}}>Hello whats Going On!</h2>
+    <h2 style={{textAlign:'center', marginTop:50}}>I would love to change this.</h2>
     
     </>
    
