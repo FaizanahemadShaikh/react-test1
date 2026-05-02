372a12d9b596f0450958e9df966f39cab5b8cc92
ShaikhFaizan2006 <engg.shaikhfaizan@gmail.com>
2026-05-02 16:21:07 +0530

    Push files to git

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+    <h1 style={{fontSize:100, textAlign:'center', marginTop:600}}> Lets Learn Together</h1>
+    <h2 style={{textAlign:'center', marginTop:50}}>Hello whats Going On!</h2>
+    
+    </>
+   
+  );
+}
+
+export default App
